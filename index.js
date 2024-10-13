e7257b6782b2dbcc855e409f6d626438cd08af37
Benny Yeung <benny.tf@gmail.com>
2024-10-13 23:39:38 +0100

    Layout complete

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ window.addEventListener("scroll", function () {
     }
 });
  
-
+document.getElementById("contactBtn").addEventListener("click", function () {
+    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+});
